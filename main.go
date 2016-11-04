4f5d0d976b2ed13674b8397766f52830f5fbd68c
xime <ximebcn@yahoo.es>
2016-11-04 22:20:17 +0100

    First implementation. Proof of concept. No validation. IT WORKS!!!!

--- main.go
+++ main.go
@@ -4,20 +4,25 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 //go build -x github.com/x1m3/xtreamer
 
 func serveVideoAction(w http.ResponseWriter, request *http.Request) {
-	w.Header().Set("Content-Type", "video/mp4")
-	w.Write([]byte("hola"))
-
-
+	basepath:=`C:\Users\xime\Desktop\`
+	vars := mux.Vars(request)
+	filename := basepath + vars["filename"]
+	fp,_ := os.Open(filename)
+	http.ServeContent(w, request, filename, time.Now(),fp)
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/lala", serveVideoAction).Methods("GET")
+	router.HandleFunc(`/stream/{filename:[a-zA-Z0-9\/\._]+}`, serveVideoAction).Methods("GET")
+
+
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
